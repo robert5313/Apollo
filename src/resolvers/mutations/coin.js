ac6b6cb503cd4f62c0450ebd9346687bb8b8a91c
robert5313 <kiptoorobert@etech.ac.ke>
2022-12-04 09:36:44 +0300

    My first commit

---
+++ src/resolvers/mutations/coin.js
@@ -0,0 +1,5 @@
+const {ERC20Coin} = require('./coin');
+
+module.exports = [
+    ERC20Coin
+]
---
+++ src/resolvers/mutations/index.js
@@ -0,0 +1,7 @@
+const createCoin = require('./createCoin');
+const updateCoin = require('./updateCoin');
+const deleteCoin = require('./deleteCoin');
+
+
+
+module.exports = { createCoin, updateCoin, deleteCoin }
